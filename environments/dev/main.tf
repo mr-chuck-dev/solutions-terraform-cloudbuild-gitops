7cb682dfaea6335cd47230c7cbdfdfa7909e06e8
Chuck Peters <mr.chuck@frankencloud.com>
2023-03-20 20:35:01 +0000

    making new policy

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -42,9 +42,10 @@ module "firewall" {
 }
 
 resource "google_compute_firewall_policy" "test_policy" {
-  parent	= "organizations/490866986856"
-  short_name	= "testpolicy"
-  description	= "Test Policy"
+  parent		= "organizations/490866986856"
+  short_name		= "testpolicy"
+  description		= "Test Policy"
+  target_resources	= "${module.vpc.subnet}"
 }
 
 resource "google_compute_firewall_policy_rule" "test_policy_rule" {
@@ -55,7 +56,6 @@ resource "google_compute_firewall_policy_rule" "test_policy_rule" {
   action 		= "allow"
   direction 		= "INGRESS"
   disabled 		= false
-  target_resources	= "${module.vpc}"
   match {
     layer4_configs {
       ip_protocol = "tcp"
